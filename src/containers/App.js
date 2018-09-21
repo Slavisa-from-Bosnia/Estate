6a23e19657074b0de51ecaed586ee4cb27c9022f
Slavisa Babic <babic.slavisa@gmail.com>
2018-09-21 13:27:28 +0200

    google maps

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import ImgMediaCard from '../components/cards';
+import GoogleMapLoader from '../components/googleMaps';
 
 class App extends Component {
   render() {
@@ -10,6 +12,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <ImgMediaCard/>
+        <ImgMediaCard/>
+       <GoogleMapLoader></GoogleMapLoader>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
