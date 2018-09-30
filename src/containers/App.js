c41771ddff0bf37748fe4399faef56057db4db74
Slavisa Babic <babic.slavisa@gmail.com>
2018-09-30 18:01:53 +0200

    dizajn

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import ImgMediaCard from '../components/cards';
 import ButtonAppBar from '../components/appbar';
-
 import Map from '../components/googleMaps';
-
-
+//SET PATH=C:\Program Files\Nodejs;%PATH%
+import FullWidthGrid from '../components/FullWidthGrid';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <ButtonAppBar/>
-        <ImgMediaCard/>
-        <ImgMediaCard/>
+        <FullWidthGrid/>
          <Map/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
